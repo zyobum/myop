0d8f2ac15c897a61b296b44ede67374346ccd709
zyobum <zyobum@5068.info>
2020-07-22 21:26:57 +0800

    Update patch.sh
    update the path of driver monitoring

--- patch.sh
+++ patch.sh
@@ -1,4 +1,4 @@
 # /bin/bash
 cd $(dirname "$0")
-patch /data/openpilot/selfdrive/controls/lib/driver_monitor.py driver_monitor.py.patch
+patch /data/openpilot/selfdrive/monitoring/driver_monitor.py driver_monitor.py.patch
 patch /data/openpilot/selfdrive/car/toyota/carcontroller.py carcontroller.py.patch
